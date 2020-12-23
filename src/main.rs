f952c056cb3869c0fef30d1f36b359f6bdfad2d8
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-12-23 19:57:18 +0100

    read a file using tokio

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use tokio::net::{TcpListener, TcpStream};
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt};
 
 #[tokio::main]
-async fn main() {}
+async fn main() -> io::Result<()> {
+    let mut f = File::open("foo.txt").await?;
+    // read up to 10 bytes
+    let mut buffer = [0; 10];
+    let n = f.read(&mut buffer[..]).await?;
+
+    println!("The bytes: {:?}", &buffer[..n]);
+    Ok(())
+}
