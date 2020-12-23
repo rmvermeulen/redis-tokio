6a0da5e9bff2d52a82b706fdf505d6c5b0050586
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-12-23 18:10:47 +0100

    Implement step 1 of tokio tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    // open a connection to mini-redis
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // set value
+    client.set("hello", "world".into()).await?;
+
+    // get value
+    let result = client.get("hello").await?;
+
+    print!("got value '{:?}' for key 'hello'", result);
+    Ok(())
 }
